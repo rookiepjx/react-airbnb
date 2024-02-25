6ed6db890e21cfa60483abf20cbbcce81a298970
rookiepjx <1254198616@qq.com>
2024-02-25 19:09:39 +0800

    feat:普通方式和Rtk方式配置redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,13 @@
-const store = {};
+import { configureStore } from "@reduxjs/toolkit";
+
+import homeReducer from "./modules/home";
+import entireReducer from './modules/entire'
+
+const store = configureStore({
+  reducer: {
+    home: homeReducer,
+    entire:entireReducer
+  },
+});
 
 export default store;
