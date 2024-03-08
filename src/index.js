3b3aad20db201c9b6eb72688b47e71d364a1f3a6
rookiepjx <1254198616@qq.com>
2024-03-08 21:09:56 +0800

    feat:styled-components增加theme主题

--- src/index.js
+++ src/index.js
@@ -5,17 +5,21 @@ import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import "normalize.css";
 import "./assets/css/index.less";
-import store from './store'
+import store from "./store";
+import { ThemeProvider } from "styled-components";
+import theme from "./assets/theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <Suspense fallback={<div>loading...</div>}>
-    <HashRouter>
-      <App />
-    </HashRouter>
-    </Suspense>
+      <ThemeProvider theme={theme}>
+        <Suspense fallback={<div>loading...</div>}>
+          <HashRouter>
+            <App />
+          </HashRouter>
+        </Suspense>
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
