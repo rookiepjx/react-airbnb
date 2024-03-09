420175733fc99d7d250f0ab592ccb212659ac71c
rookiepjx <1254198616@qq.com>
2024-03-10 00:43:28 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ git add -A
 git commit -m 'deploy'
  
 # 如果你想要部署到 https://USERNAME.github.io
-git push -f https://github.com/rookiepjx/react-airbnb master
+git push -f https://github.com/rookiepjx/react-airbnb.git master
  
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
