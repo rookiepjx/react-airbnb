ce23c8c045f11bdd244500e0573a0d763313e9c7
rookiepjx <1254198616@qq.com>
2024-03-12 21:09:26 +0800

    feat:查看更多跳转详情

--- src/components/section-footer/index.jsx
+++ src/components/section-footer/index.jsx
@@ -1,16 +1,23 @@
 import IconMoreArrow from "@/assets/svg/icon-more-arrow";
 import PropTypes from "prop-types";
 import React, { memo } from "react";
+import { useNavigate } from "react-router-dom";
 import { FooterWrapper } from "./style";
 
 const SectionFooter = memo((props) => {
   const { name } = props;
 
   const text = name ? `显示更多${name}房源` : "显示全部";
+  const navigate = useNavigate();
+
+  // 点击更多跳转详情
+  const moreClickHandle = () => {
+    navigate("/entire");
+  };
 
   return (
     <FooterWrapper color={name ? "#00848A" : "#000"}>
-      <div className="info">
+      <div className="info" onClick={moreClickHandle}>
         <span className="text">{text}</span>
         <IconMoreArrow />
       </div>
