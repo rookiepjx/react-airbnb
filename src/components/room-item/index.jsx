08b7f6d9ff7fe9d6bc298a6465ab69a5361ca3a5
rookiepjx <1254198616@qq.com>
2024-03-21 19:00:33 +0800

    feat:room-item组件增加轮播图滚动效果

--- src/components/room-item/index.jsx
+++ src/components/room-item/index.jsx
@@ -1,14 +1,57 @@
 import PropTypes from "prop-types";
-import React, { memo } from "react";
+import React, { memo, useRef } from "react";
 import { Rating } from "@mui/material";
+import { Carousel } from "antd";
+
 import { ItemWrapper } from "./style";
+import IconArrowLeft from "@/assets/svg/icon-arrow-left";
+import IconArrowRight from "@/assets/svg/icon-arrow-right";
 
 const RoomItem = memo((props) => {
   const { itemData, col, itemClick } = props;
+  const sliderRef = useRef();
 
+  // 定义事件处理函数
   const itemClickHandle = () => {
     if (itemClick) itemClick(itemData);
   };
+
+  const controlClickHandle = (e, isRight) => {
+    // 调用轮播图组件方法切换上一张、下一张
+    e.stopPropagation();
+    const slider = sliderRef.current;
+    isRight ? slider.next() : slider.prev();
+  };
+
+  // 根据返回数据结构决定展示单个图片还是图片轮播图
+  // 1、图片元素
+  const pictureElement = (
+    <div className="cover">
+      <img src={itemData.picture_url} alt="" />
+    </div>
+  );
+  // 2、轮播图元素
+  const sliderElement = (
+    <div className="slider">
+      <div className="control">
+        <div className="btn left" onClick={(e) => controlClickHandle(e, false)}>
+          <IconArrowLeft width="30" height="30" />
+        </div>
+        <div className="btn right" onClick={(e) => controlClickHandle(e, true)}>
+          <IconArrowRight width="30" height="30" />
+        </div>
+      </div>
+      <Carousel dots={false} ref={sliderRef}>
+        {itemData?.picture_urls?.map((item) => {
+          return (
+            <div className="cover" key={item}>
+              <img src={item} alt="" />
+            </div>
+          );
+        })}
+      </Carousel>
+    </div>
+  );
 
   return (
     <ItemWrapper
@@ -17,9 +60,7 @@ const RoomItem = memo((props) => {
       onClick={itemClickHandle}
     >
       <div className="inner">
-        <div className="cover">
-          <img src={itemData.picture_url} alt="" />
-        </div>
+        {!itemData.picture_urls ? pictureElement : sliderElement}
         <div className="desc">{itemData.verify_info.messages.join(" · ")}</div>
         <div className="name">{itemData.name}</div>
         <div className="price">¥{itemData.price}/晚</div>
