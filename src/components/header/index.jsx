97c58fe8c720a70310e122632279b91003d9855c
rookiepjx <1254198616@qq.com>
2024-03-26 12:23:02 +0800

    style:appHeader固定样式开发

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,15 +1,37 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
+import { shallowEqual, useSelector } from "react-redux";
+import classNames from "classnames";
+
 import { HeaderWrapper } from "./style";
 import HeaderLeft from "./components/header-left";
 import HeaderCenter from "./components/header-center";
 import HeaderRight from "./components/header-right";
 
 const Header = memo(() => {
+  // 定义内部状态
+  const [isSearch, setIsSearch] = useState(false);
+  // 从redux获取数据
+  const { headerConfig } = useSelector(
+    (state) => ({
+      headerConfig: state.main.headerConfig,
+    }),
+    shallowEqual
+  );
+  const { isFixed } = headerConfig;
+
   return (
-    <HeaderWrapper>
-      <HeaderLeft />
-      <HeaderCenter />
-      <HeaderRight />
+    <HeaderWrapper className={classNames({ fixed: isFixed })}>
+      <div className="content">
+        <div className="top">
+          <HeaderLeft />
+          <HeaderCenter />
+          <HeaderRight />
+        </div>
+      </div>
+
+      {isSearch && (
+        <div className="cover" onClick={(e) => setIsSearch(false)}></div>
+      )}
     </HeaderWrapper>
   );
 });
--- src/components/header/style.js
+++ src/components/header/style.js
@@ -1,8 +1,31 @@
 import styled from "styled-components";
 
 export const HeaderWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  height: 80px;
-  border-bottom: 1px solid #eee;
+  &.fixed {
+    position: fixed;
+    z-index: 99;
+    top: 0;
+    left: 0;
+    right: 0;
+  }
+  .content {
+    position: relative;
+    background-color: rgba(255, 255, 255, 1);
+    .top {
+      display: flex;
+      align-items: center;
+      height: 80px;
+    }
+  }
+
+  /* 遮罩层 */
+  .cover {
+    position: fixed;
+    z-index: 9;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, 0.3);
+  }
 `;
