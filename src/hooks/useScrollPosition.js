2dcb5e89624b78d70a10c00056073e841ae66230
rookiepjx <1254198616@qq.com>
2024-03-27 17:22:38 +0800

    feat:增加滚动位置hook

---
+++ src/hooks/useScrollPosition.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import { throttle } from "underscore";
+
+export const useScrollPosition = () => {
+  const [scrollX, setScrollX] = useState(0);
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = throttle(() => {
+      setScrollX(window.screenX);
+      setScrollY(window.screenY);
+    }, 100);
+
+    window.addEventListener("scroll", handleScroll);
+
+    // 注销监听
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return { scrollX, scrollY };
+};
